754f8fb0be6c3b72c50554963c4167e450824821
Samyr Melo <samyrdiamantino591@gmail.com>
2025-06-04 15:21:35 -0300

    atualização curriculo

--- src/Development/Development.jsx
+++ src/Development/Development.jsx
@@ -10,8 +10,8 @@ function Development () {
                 <h1 className='development__title'>DESENVOLVEDOR <span>WEB</span></h1>
                 <h4 className='development__subtitle'>Desenvolvo arquiteturas web robustas (React, Node.js, APIs RESTful) com UX estratégico, performance otimizada e funcionalidades escaláveis, transformando objetivos de negócio em resultados mensuráveis.</h4>
                 <div className='general__links__development'>
-                    <a className='links__development_one' href="#">Hire me</a>
-                    <a className='links__development_two' href="#">Dowload CV</a>
+                    <a className='links__development_one' href="https://www.linkedin.com/in/devsamyrmelo/">Hire me</a>
+                    <a className='links__development_two' href="https://docs.google.com/document/d/13hjonXC_YYpNsPV7WIfL7KGQVUAfqmi8/edit?usp=sharing&ouid=116137133917629393315&rtpof=true&sd=true">Dowload CV</a>
                 </div>
 
             </div>
